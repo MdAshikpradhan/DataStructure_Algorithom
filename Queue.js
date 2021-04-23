ab2604f8829855e7b06f9d7e9056bcc9f3d3f1b3
mdAshikPradhan <mdashikpradhan.com>
2021-04-23 16:25:26 +0600

    update

---
+++ Queue.js
@@ -0,0 +1,30 @@
+// In the name of Allah 
+// There is no God but Allah and Mohammad (S) is the Messanger of Allah.
+class Queue {
+    constructor() {
+        this.queue = [];
+    }
+    enqueue(item) {
+        this.queue.push(item);
+    }
+    dequeue() {
+        if(this.queue.length) {
+            return this.queue.shift()
+        }
+    }
+}
+
+const brothers = new Queue();
+
+brothers.enqueue('Alif')
+brothers.enqueue('Mahim')
+brothers.enqueue('Sidan')
+brothers.enqueue('Shakin')
+
+console.log(brothers.queue)
+
+const removeBrother = brothers.dequeue();
+
+console.log(removeBrother)
+
+console.log(brothers.queue);
---
+++ Stack.js
@@ -0,0 +1,25 @@
+class Stack {
+    constructor() {
+        this.stack = [];
+    }
+    add(item) {
+        this.stack.push(item)
+    }
+    remove() {
+        if(this.stack.length) {
+            this.stack.pop()
+        }
+    }
+}
+
+const brothers = new Stack();
+
+brothers.add('Alif')
+brothers.add('Shakin')
+brothers.add('Sidan')
+brothers.add('Mahim')
+console.log(brothers)
+
+const removeBrother = brothers.remove()
+console.log(removeBrother)
+console.log(brothers.stack)
